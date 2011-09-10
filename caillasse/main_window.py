ace076d86b50792e8dace3df6240c1ea1617f57e
Feth AREZKI <feth@tuttu.info>
2011-09-10 08:41:26 +0200

    wip main window. uses correcty transfers, persons and expenees

--- caillasse/main_window.py
+++ caillasse/main_window.py
@@ -1,10 +1,10 @@
 from PyQt4.QtCore import QObject, Qt, SIGNAL, pyqtSignal
-from PyQt4.QtGui import QAction, QMainWindow
+from PyQt4.QtGui import QAction, QMainWindow, QMessageBox
 
 from velat import Velat
 
 from .main_window_ui import Ui_caillasse
-from .models import PersonsModel
+from .models import ExpensesModel, PersonsModel, TransfersModel 
 
 
 def _connect(source, target):
@@ -15,8 +15,6 @@ def _connect(source, target):
 
 class Caillasse(QMainWindow, Ui_caillasse):
 
-    new_person = pyqtSignal(name="new_person")
-
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
@@ -24,13 +22,15 @@ class Caillasse(QMainWindow, Ui_caillasse):
         self._file = None
         self._velat = None
         self._all_persons_model = None
-        self._expenses_model = None
+        self._all_expenses_model = None
+        self._all_transfers_model = None
 
         _connect(self.actionNew, self._new)
         _connect(self.actionSave, self._save)
         _connect(self.actionSave_copy, self._save_as)
         _connect(self.actionNew_person, self._new_person)
         _connect(self.actionNew_expense, self._new_expense)
+        _connect(self.actionNew_transfer, self._new_transfer)
 
         self._new()
 
@@ -39,6 +39,10 @@ class Caillasse(QMainWindow, Ui_caillasse):
         self._velat = Velat()
         self._all_persons_model = PersonsModel(self._velat, self)
         self.all_persons.setModel(self._all_persons_model)
+        self._all_expenses_model = ExpensesModel(self._velat, self)
+        self.all_expenses.setModel(self._all_expenses_model)
+        self._all_transfers_model = TransfersModel(self._velat, self)
+        self.all_transfers.setModel(self._all_transfers_model)
 
     def _save(self):
         print "save"
@@ -49,7 +53,17 @@ class Caillasse(QMainWindow, Ui_caillasse):
         print "save as"
     
     def _new_person(self):
-        self._all_persons_model.new_person(self)
+        self.tabs.setCurrentIndex(0)
+        self._all_persons_model.new_item(self)
 
     def _new_expense(self):
-        self._all_persons_model.new_person(self)
+        self.tabs.setCurrentIndex(1)
+        self._all_expenses_model.new_item(self)
+
+    def _new_transfer(self):
+        if not self._velat.persons:
+            QMessageBox.warning(self, "New transfer", "Please create persons "
+                    "before transfers between them")
+            return
+        self.tabs.setCurrentIndex(2)
+        self._all_transfers_model.new_item(self, self._all_persons_model)
